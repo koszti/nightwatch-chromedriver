8b8362ab9d81e6c6a9e99b130ae9d9fa8e5acf46
Peter Kosztolanyi <peter.kosztolanyi@quidco.com>
2017-04-26 12:02:16 +0100

    More log in nighwatch globals

--- globals.js
+++ globals.js
@@ -1,11 +1,13 @@
 var chromedriver = require('chromedriver');
 module.exports = {
   before : function(done) {
+    console.log("Starting chromedriver...");
     chromedriver.start();
     done();
   },
 
   after : function(done) {
+    console.log("Stopping chromedriver...");
     chromedriver.stop();
     done();
   }
